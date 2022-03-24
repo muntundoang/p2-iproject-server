0e8c943420b19934521820122abf4faca15a8a23
Rio Lukman Tawekal <riolukmant@gmail.com>
2022-03-25 05:23:30 +0700

    get list for querying lyric

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const cors = require('cors')
 const Controller = require('./controllers/controller')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', Controller.getVideos)
+app.get('/video', Controller.getVideos)
+app.get('/lyric', Controller.getListLyric)
+app.get('/lyric/:track_id', Controller.getLyric)
 
 
 app.listen(port, () => {
